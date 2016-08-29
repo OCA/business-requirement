f8ff39c1f81d2dce09fd0b46964c8c24166cd7d1
Evan Li <li.aiwen@elico-corp.com>
2016-08-29 11:35:16 +0800

    [WIP]business_requirement_deliverable_report: add paper format to reduce the space under the header, change the precision, right justfied some columns, delete unnecessary py files

--- business_requirement_deliverable_report/report/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import business_requirement_report
-from . import business_requirement_deliverable_report
-from . import business_requirement_deliverable_resource_report
