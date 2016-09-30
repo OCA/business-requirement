a6a4abbf9abb1aafb2755702d9e049558d312d0c
Sudhir Arya <sudhir.arya@serpentcs.com>
2016-09-30 12:37:32 +0530

    [ADD] Migrated business_requirement_deliverable_categ module from v8 to v9.

---
+++ business_requirement_deliverable_categ/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2016 Elico Corp (www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Business Requirement Resources Task Categories",
+    'category': 'Business Requirement Management',
+    'summary': 'Adds Task Categories to your Business Requirement Resources',
+    "version": "9.0.1.0.0",
+    "website": "www.elico-corp.com",
+    "author": "Elico Corp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "project_categ",
+        "business_requirement_deliverable"
+    ],
+    "data": [
+        "views/business_requirement_deliverable_categ.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
