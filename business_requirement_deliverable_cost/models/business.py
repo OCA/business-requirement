6d0d9cd16ce99a09d29b34f681a868de2ac69b57
Victor Martin <victor.martin@elico-corp.com>
2016-12-13 16:30:27 +0800

    [FIX] business_requirement_deliverable_cost: Fix the issue using properly knowledge of new API about many2one. 	modified:   business_requirement_deliverable_cost/models/business.py

--- business_requirement_deliverable_cost/models/business.py
+++ business_requirement_deliverable_cost/models/business.py
@@ -46,11 +46,8 @@ class BusinessRequirementResource(models.Model):
     @api.multi
     def _get_partner(self):
         self.ensure_one()
-        br_id = br_deliverable = False
-        if self.business_requirement_deliverable_id:
-            br_deliverable = self.business_requirement_deliverable_id
-        if br_deliverable.business_requirement_id:
-            br_id = br_deliverable.business_requirement_id
+        br_id = self.business_requirement_deliverable_id.\
+            business_requirement_id.id
         if br_id and br_id.partner_id:
             return br_id.partner_id
         else:
