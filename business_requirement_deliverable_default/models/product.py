637120ea30131e0b85a50a1bd48adfbf5d142d11
Sudhir Arya <sudhir.arya@serpentcs.com>
2017-02-27 18:28:40 +0530

    [IMP] Removed unused imports

--- business_requirement_deliverable_default/models/product.py
+++ business_requirement_deliverable_default/models/product.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2016 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import api, fields, models
+from openerp import fields, models
 
 
 class ProductTemplate(models.Model):
