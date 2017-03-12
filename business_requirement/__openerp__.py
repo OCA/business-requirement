11b48b6dd6c2e97a5c44bb9f97dd2f99fd45cf5b
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-03-12 11:09:27 +0100

    [UPD] fix website url in manifest

--- business_requirement/__openerp__.py
+++ business_requirement/__openerp__.py
@@ -7,7 +7,7 @@
     'summary': 'Manage the Business Requirements (stories, scenarios, gaps\
         and test cases) for your customers',
     'version': '8.0.5.1.2',
-    'website': 'www.elico-corp.com',
+    'website': 'https://www.elico-corp.com/',
     "author": "Elico Corp, Odoo Community Association (OCA)",
     'depends': [
         'product',
