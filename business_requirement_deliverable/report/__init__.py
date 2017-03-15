7773a2f390dc071cdc0f391d6cc05f18a452d9c9
darshan-serpent <darshan.patel.serpentcs@gmail.com>
2017-03-15 17:47:26 +0530

    [FIX] pylint

--- business_requirement_deliverable/report/__init__.py
+++ business_requirement_deliverable/report/__init__.py
@@ -2,5 +2,5 @@
 # © 2016 Elico Corp (www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import br_deliverable_sale_report
-import br_deliverable_report
+from . import br_deliverable_sale_report
+from . import br_deliverable_report
