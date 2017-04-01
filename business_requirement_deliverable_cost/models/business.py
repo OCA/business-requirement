7f0ecd1710daba5383b46545fe7b5d269ef93792
darshan-serpent <darshan.patel.serpentcs@gmail.com>
2017-04-01 14:45:38 +0530

    [IMP] Transfer the Gross Profit fields to the Deliverable lines

--- business_requirement_deliverable_cost/models/business.py
+++ business_requirement_deliverable_cost/models/business.py
@@ -123,6 +123,24 @@ class BusinessRequirementDeliverable(models.Model):
         groups='business_requirement_deliverable_cost.'
         'group_business_requirement_estimation',
     )
+    gross_profit = fields.Float(
+        compute='_compute_gross_profit',
+        string='Gross Profit',
+    )
+
+    @api.multi
+    @api.depends('resource_ids.price_total',
+                 'resource_ids.sale_price_total')
+    def _compute_gross_profit(self):
+        for brd in self:
+            if brd.resource_ids:
+                price_total = 0
+                sale_price = 0
+                for res in brd.resource_ids:
+                    price_total += res.price_total
+                    sale_price += res.sale_price_total
+                brd.gross_profit = price_total - sale_price
+            brd.resource_ids = 0.0
 
     @api.multi
     def action_button_update_estimation(self):
