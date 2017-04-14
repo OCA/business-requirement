6aa206f9a745299c73fb233efa4df9581fbeb606
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-04-14 06:46:25 +0530

    [8.0][ADD] BI report for the business requirements (#86)
    * [ADD] BI report for the business requirements
    
    * [FIX] pylint
    
    * [IMP] Added Access rights
    
    * [FIX] travis
    
    * [FIX] made requested changes
    
    * [FIX] Fixed travis
    
    * [IMP] Added Br report file and improved code
    
    * [IMP] Changed as per comments.
    
    * [IMP] Improved code.
    
    * [IMP]Improved SQL readabilityby submethods.
    
    * [IMP]Improved Indentation.

--- business_requirement_deliverable/__openerp__.py
+++ business_requirement_deliverable/__openerp__.py
@@ -17,6 +17,8 @@
         'security/business_requirement_deliverable_security.xml',
         'security/ir.model.access.csv',
         'views/business_view.xml',
+        'report/br_deliverable_report_view.xml',
+        'report/br_deliverable_sale_report_view.xml',
     ],
     'image': [
         'static/description/icon.png',
