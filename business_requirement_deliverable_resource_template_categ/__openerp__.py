2777a42a5376fc70969a157e2d9f8b9fff4207d0
Victor Martin - Elico Corp <victor.martin@elico-corp.com>
2017-05-16 11:35:57 +0800

    [ADD] business_requirement_deliverable_resource_template_categ (#136)
    * [ADD] business_requirement_deliverable_resource_template_categ
    - Extend Resources Templates with Task Category Main feature
    
    * [FIX] business_requirement_deliverable_resource_template_categ
    - Fix manifest and Readme files
    
    * [FIX] business_requirement_deliverable_resource_template_categ
    - Fix XML format

---
+++ business_requirement_deliverable_resource_template_categ/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2017 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Business Requirement Deliverable Resource Template Category',
+    'category': 'Business Requirements Management',
+    'summary': """Manage default resource lines categories in your
+               deliverable templates""",
+    'version': '8.0.1.0.0',
+    'website': 'https://www.elico-corp.com/',
+    "author": "Elico Corp, Odoo Community Association (OCA)",
+    'depends': [
+        'business_requirement_deliverable_project_task_categ',
+        'business_requirement_deliverable_resource_template',
+    ],
+    'image': [
+        'static/description/icon.png',
+        'static/img/bus_req_default.png',
+        'static/img/bus_req_default2.png'
+    ],
+    'data': [
+        "views/business_requirement_resource_template.xml",
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+}
