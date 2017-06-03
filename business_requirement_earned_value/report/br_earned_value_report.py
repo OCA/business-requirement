ed79664c6f27722e19aed9567dd9c6799293a6e4
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-06-03 12:08:37 +0530

    [FIX] - 8.0 issue 152 Earned Value Analysis (#163)
    * [FIX]division by zero
    
    * [IMP]Improved code
    
    * [IMP]Improved Code
    
    * [FIX] indentation as suggested by @seb-elico

--- business_requirement_earned_value/report/br_earned_value_report.py
+++ business_requirement_earned_value/report/br_earned_value_report.py
@@ -88,15 +88,21 @@ class BusinessRequirementEarnedValueReport(models.Model):
                          WHERE
                              pt.business_requirement_id = br.id) > 0
                     THEN
-                        (abs(((SELECT
-                                   SUM(pt.effective_hours)
-                               FROM
-                                   project_task pt
-                               WHERE
-                                   pt.business_requirement_id = br.id
-                               ) * ptm.list_price) - (SUM(res.qty
-                               ) * SUM(res.unit_price))) /
-                                SUM(res.unit_price))
+                        CASE
+                            WHEN
+                              SUM(res.unit_price) <> 0
+                            THEN
+                                abs(((SELECT SUM(pt.effective_hours)
+                                FROM
+                                    project_task pt
+                                WHERE
+                                    pt.business_requirement_id = br.id
+                                ) * ptm.list_price) - (SUM(res.qty
+                                ) * SUM(res.unit_price))) /
+                                SUM(res.unit_price)
+                            ELSE
+                                0.0
+                            END
                 ElSE 0.0 END AS per_variances,
                 CASE
                     WHEN
