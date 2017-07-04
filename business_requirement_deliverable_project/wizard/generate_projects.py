02f157a9a5e9693f24923ed0697db7ea59eca0e7
Sudhir Arya <sudhir.arya@serpentcs.com>
2017-07-04 16:32:25 +0530

    [IMP]Improved code

--- business_requirement_deliverable_project/wizard/generate_projects.py
+++ business_requirement_deliverable_project/wizard/generate_projects.py
@@ -137,8 +137,10 @@ class BrGenerateProjects(models.TransientModel):
         if self.for_childs:
             br_project = br_project or parent_project
             for child_br in br.business_requirement_ids:
-                self.generate_br_projects(
-                    br_project, child_br, project_ids, task_ids)
+                if child_br.state == 'stakeholder_approval' and \
+                        child_br.linked_project:
+                    self.generate_br_projects(br_project, child_br,
+                                              project_ids, task_ids)
 
     @api.multi
     def generate_deliverable_projects(
