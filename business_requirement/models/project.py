158e8c86d35d972df200d1767dc38e9ef33320b7
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-07-24 15:53:12 +0530

    [IMP][8.0][22070][ISSUE#115]-Modify projects and BR id relationship (#192)
    * [IMP][8.0][22070][ISSUE#115]-Improved code as per changes
    
    * [IMP]Improved domain and added copy=False in linked_project
    
    * [ADD/IMP]Linked Project with BR and DL with Smartbutton
    
    * [IMP]Improved code
    
    * [IMP]Count number of linked project in BR,removed count function and veriable from project
    
    * [IMP]Improved method name
    
    * [FIX]Fix Travis
    
    * [IMP]Improved code
    
    * [IMP]Improved code
    
    * [IMP] Improved method name to field

--- business_requirement/models/project.py
+++ business_requirement/models/project.py
@@ -2,7 +2,7 @@
 # © 2016 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from openerp import fields, models
 
 
 class Project(models.Model):
@@ -14,12 +14,3 @@ class Project(models.Model):
         string='Business Requirements',
         copy=False,
     )
-    br_count = fields.Integer(
-        compute='_compute_br_count',
-        string="Number of Business Requirements"
-    )
-
-    @api.one
-    @api.depends('br_ids')
-    def _compute_br_count(self):
-        self.br_count = len(self.br_ids)
