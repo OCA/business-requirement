f838a761a5d646cf55e410be8938f12b980fed46
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2017-08-01 11:57:54 +0530

    [IMP]Improved code

--- business_requirement_deliverable_crm/wizard/crm_make_sale.py
+++ business_requirement_deliverable_crm/wizard/crm_make_sale.py
@@ -20,54 +20,26 @@ class CrmMakeSale(models.TransientModel):
         'Customer',
         default=default_partner
     )
-    update_quotation = fields.Boolean('Update existing quotation')
 
     @api.multi
-    def make_orderline(self):
-        print "111111111111111111111111111111111111111111"
+    def make_order(self):
         context = self.env.context
-        print "ccccccccc",context
         case_id = context and context.get('active_ids', []) or []
-        print "case_idcase_idcase_id",case_id
         case_id = case_id and case_id[0] or False
         crm_id = self.env['crm.lead'].browse(case_id)
-        print "resresres",case_id
         if crm_id and crm_id.project_id:
             partner = crm_id.partner_id
             sale_order = self.env['sale.order']
-            partner_obj = self.env['res.partner']
-            pricelist = partner.property_product_pricelist.id
-            partner_addr = partner.address_get(['default', 'invoice', 'delivery', 'contact'])
-            payment_term = partner.property_payment_term_id and partner.property_payment_term_id.id or False
-            if not partner and crm_id.partner_id:
-                partner = crm_id.partner_id
-                fpos = partner.property_account_position_id and partner.property_account_position.id or False
-                payment_term = partner.property_payment_term_id and partner.property_payment_term_id.id or False
-                partner_addr = partner_obj.address_get(
-                    [partner.id],
-                    ['default', 'invoice', 'delivery', 'contact']
-                )
-                pricelist = partner.property_product_pricelist.id
+            pricelist = partner.property_product_pricelist.id or False
             sale_order_vals = {
                 'partner_id': partner.id,
                 'opportunity_id': crm_id.id,
-                # 'categ_ids': [
-                #     (6, 0, [categ_id.id for categ_id in crm_id.categ_ids])],
                 'pricelist_id': pricelist,
-                'partner_invoice_id': partner_addr['invoice'],
-                'partner_shipping_id': partner_addr['delivery'],
                 'date_order': fields.datetime.now(),
                 }
-
-            print "sale_order_vals",sale_order_vals
             order_id = sale_order.create(sale_order_vals)
-            print "order_idorder_idorder_id",order_id
             order_lines = self.prepare_sale_order_line(case_id, order_id)
             self.create_sale_order_line(order_lines)
-            message = _(
-                "Opportunity has been <b>converted</b> to the quotation <em>%s</em>.") % (
-                      sale_order.name)
-            crm_id.message_post(body=message)
             return {
                 'domain': str([('id', 'in', [order_id.id])]),
                 'view_type': 'form',
@@ -111,5 +83,4 @@ class CrmMakeSale(models.TransientModel):
     def create_sale_order_line(self, order_lines):
         saleorder_line_obj = self.env['sale.order.line']
         for line in order_lines:
-            print "linelinelineline",line
             saleorder_line_obj.create(line)
