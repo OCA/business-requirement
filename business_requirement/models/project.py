16f121e26a58b8815c98013a5b1357db10035539
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2017-08-16 04:48:46 +0530

    [MIG]Migrated Business requirement module from 8.0 to 10.0 (#223)
    * [MIG]Migrated Business requirement module from 8.0 to 10.0
    
    * [FIX]Fix Pylint
    
    * [IMP]Added new blankline in security file
    
    * [IMP]Added Test-Cases for Coverage
    
    * [IMP]Added report,improved fields in py and xml
    
    * [IMP]Improved code
    
    * [IMP] Removed default dimensions from pivot view
    
    * [FIX]Fixed Pylint
    
    * [IMP]Improved code
    
    * [IMP]Improved code
    
    * [FIX]Fixed Travis
    
    * [IMP]Test-cases Improved
    
    * [IMP]Improved view as per OCA standards
    
    * [IMP]Improved assert in test-cases
    
    * [IMP]-Added assertEqual in test-cases
    
    * [IMP]Improved Test-cases
    
    * [IMP]Improved Code as per OCA Standard in XML
    
    * [IMP/REM]Cover whole BR-code and removed unnecessary test-cases.
    
    * [IMP]Cover test-cases default_comany function

--- business_requirement/models/project.py
+++ business_requirement/models/project.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2016 Elico Corp (https://www.elico-corp.com).
+# © 2017 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class Project(models.Model):
