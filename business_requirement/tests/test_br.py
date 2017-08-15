67429f03204dace547688143d4f22082235ea718
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2017-08-16 04:48:46 +0530

    [MIG]Migrated Business requirement module from 8.0 to 10.0 (#223)
    * [MIG]Migrated Business requirement module from 8.0 to 10.0
    
    * [FIX]Fix Pylint
    
    * [IMP]Added new blankline in security file
    
    * [IMP]Added Test-Cases for Coverage
    
    * [IMP]Added report,improved fields in py and xml
    
    * [IMP]Improved code
    
    * [IMP] Removed default dimensions from pivot view
    
    * [FIX]Fixed Pylint
    
    * [IMP]Improved code
    
    * [IMP]Improved code
    
    * [FIX]Fixed Travis
    
    * [IMP]Test-cases Improved
    
    * [IMP]Improved view as per OCA standards
    
    * [IMP]Improved assert in test-cases
    
    * [IMP]-Added assertEqual in test-cases
    
    * [IMP]Improved Test-cases
    
    * [IMP]Improved Code as per OCA Standard in XML
    
    * [IMP/REM]Cover whole BR-code and removed unnecessary test-cases.
    
    * [IMP]Cover test-cases default_comany function

--- business_requirement/tests/test_br.py
+++ business_requirement/tests/test_br.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2016 Elico Corp (https://www.elico-corp.com).
+# © 2017 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp.tests import common
+from odoo.tests import common
+from openerp.exceptions import ValidationError
 
 
 @common.at_install(False)
@@ -15,7 +16,7 @@ class BusinessRequirementTestCase(common.TransactionCase):
         # Configure unit of measure.
         self.categ_wtime = self.ref('product.uom_categ_wtime')
         self.categ_kgm = self.ref('product.product_uom_categ_kgm')
-
+        self.partner1 = self.ref('base.res_partner_1')
         self.UomObj = self.env['product.uom']
         self.uom_hours = self.UomObj.create({
             'name': 'Test-Hours',
@@ -35,13 +36,14 @@ class BusinessRequirementTestCase(common.TransactionCase):
             'rounding': 0.000001})
 
         self.AnalyticAccount = self.AnalyticAccountObject.create(
-            {'name': 'AnalyticAccount for Test',
-             'state': 'draft'})
+            {'name': 'AnalyticAccount for Test'})
 
         self.projectA = self.ProjectObj. \
-            create({'name': 'Test Project A', 'partner_id': 1, 'parent_id': 1,
+            create({'name': 'Test Project A', 'partner_id': 1,
                     'analytic_account_id': self.AnalyticAccount.id})
         self.br = self.env['business.requirement']
+        self.pr_1 = self.env.ref('project.project_project_1')
+        self.pr_2 = self.env.ref('project.project_project_2')
 
         vals = {
             'description': 'test',
@@ -58,9 +60,46 @@ class BusinessRequirementTestCase(common.TransactionCase):
                     'uom_po_id': self.uom_hours.id,
                     'standard_price': 550})
 
+        vals1 = vals.copy()
+        vals2 = vals.copy()
         self.brA = self.env['business.requirement'].create(vals)
-        self.brB = self.env['business.requirement'].create(vals)
-        self.brC = self.env['business.requirement'].create(vals)
+        self.brB = self.env['business.requirement'].create(vals1)
+        self.brC = self.env['business.requirement'].create(vals2)
+
+    def test_project_id_change(self):
+        self.pr_1.write({'partner_id': self.partner1})
+        self.brA.project_id_change()
+        self.assertEqual(
+            self.brA.project_id.partner_id.id, self.brA.partner_id.id
+        )
+
+    def test_compute_sub_br_count(self):
+        self.brC.write({'parent_id': self.brA.id})
+        self.brB.write({'parent_id': self.brA.id})
+        self.brA._compute_sub_br_count()
+        self.assertEqual(
+            self.brA.sub_br_count, len(self.brA.business_requirement_ids)
+        )
+
+    def test_message_post(self):
+        self.brA.with_context({
+            'default_model': 'business.requirement',
+            'default_res_id': self.brA.id
+        }).message_post(
+            body='Test Body',
+            message_type='notification',
+            subtype='mt_notification',
+            parent_id=False,
+            attachments=None,
+            content_subtype='html',
+            **{}
+        )
+
+    def test_get_default_company(self):
+        self.brA._get_default_company()
+        self.env.user.company_id = False
+        with self.assertRaises(ValidationError):
+            self.brA._get_default_company()
 
     def test_get_level(self):
         br_vals1 = {
@@ -101,8 +140,33 @@ class BusinessRequirementTestCase(common.TransactionCase):
             'parent_id': False,
         }
         self.br.create(br_vals)
+
         brs = self.br.name_search(name='test')
         self.assertEqual(bool(brs), True)
+
+    def test_create_name_equal_slash(self):
+        name = self.env['ir.sequence'].next_by_code('business.requirement')
+        br_vals = {
+            'name': '/',
+            'description': 'test',
+        }
+        len_seq = name[2:]
+        seq = "BR" + str(int(len_seq) + 1).zfill(int(len(len_seq)))
+        res = self.br.create(br_vals)
+        self.assertEqual(seq, res.name)
+
+    def test_create_write_project(self):
+        br_vals = {
+            'name': 'test',
+            'description': 'test',
+            'project_id': self.pr_1.id
+        }
+        res = self.br.create(br_vals)
+        br_vals1 = {
+            'project_id': self.pr_2.id
+        }
+        res.write(br_vals1)
+        self.assertEqual(res.project_id.id, self.pr_2.id)
 
     def test_br_read_group(self):
         self.env['business.requirement'].read_group(
