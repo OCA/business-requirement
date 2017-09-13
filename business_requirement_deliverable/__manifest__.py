53f946b609066cc45cd2d4e22abe3ad353819563
Sudhir Arya <sudhir-serpentcs@users.noreply.github.com>
2017-09-13 16:07:35 +0530

    [MIG]-Migrated Business Requirement Deliverable from 8.0 to 10.0. (#224)
    * [IMP/ADD]Added business_requirement_deliverable
    
    * [IMP]Improved code
    
    * [IMP]Improved code
    
    * [FIX]Fixed issue of graph view
    
    * [FIX]Fixed issue of graph view

--- business_requirement_deliverable/__manifest__.py
+++ business_requirement_deliverable/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# © 2016 Elico Corp (https://www.elico-corp.com).
+# © 2017 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Business Requirement Deliverable',
     'category': 'Business Requirements Management',
     'summary': 'Manage the Business Requirement Deliverables and \
                 Resources for your customers',
-    'version': '8.0.5.0.1',
+    'version': '10.0.1.0.0',
     'website': 'https://www.elico-corp.com/',
     "author": "Elico Corp, Odoo Community Association (OCA)",
     'depends': [
@@ -16,18 +16,26 @@
     'data': [
         'security/business_requirement_deliverable_security.xml',
         'security/ir.model.access.csv',
+        'data/br_report_paperformat.xml',
         'views/business_view.xml',
         'views/res_partner.xml',
+        'views/report_business_requirement_deliverable.xml',
+        'views/report_business_requirement_deliverable_resource.xml',
         'report/br_deliverable_report_view.xml',
         'report/br_deliverable_sale_report_view.xml',
+        'report/report.xml',
     ],
     'image': [
         'static/description/icon.png',
         'static/img/bus_req_deliverable.png',
         'static/img/bus_req_deliverable2.png',
-        'static/img/bus_req_resource.png'
+        'static/img/bus_req_resource.png',
+        'static/img/bus_req_report1.png',
+        'static/img/bus_req_report2.png',
+        'static/img/bus_req_report3.png',
+
     ],
     'demo': ['data/business_requirement_deliverable_demo.xml'],
     'license': 'AGPL-3',
-    'installable': False,
+    'installable': True,
 }
