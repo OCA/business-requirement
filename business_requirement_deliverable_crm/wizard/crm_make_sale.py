ca11560b45ebd4cda7b0535896da5d090acfe529
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2017-09-18 11:00:03 +0530

    [IMP]Improveed License code

--- business_requirement_deliverable_crm/wizard/crm_make_sale.py
+++ business_requirement_deliverable_crm/wizard/crm_make_sale.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# © 2016-2017 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import api, fields, models
 from odoo.tools.translate import _
 from odoo.exceptions import ValidationError
