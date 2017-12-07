ca793c83e0f6294da876717eb084e181ef11b808
xlm609 <xlm609@163.com>
2017-12-07 17:53:52 +0800

    [IMP]edit the license and add the badge

--- business_requirement_earned_value/__init__.py
+++ business_requirement_earned_value/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Elico Corp
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2017 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import report
