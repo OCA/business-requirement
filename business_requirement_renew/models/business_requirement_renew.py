d31e2b28487a1d80db14b874a1a939a3e156c34e
xlm609 <xlm609@163.com>
2017-12-22 11:56:06 +0800

    [FIX]change the menu and del the wrong fiel

--- business_requirement_renew/models/business_requirement_renew.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2017 Elico Corp (https://www.elico-corp.com).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from odoo import models, api, _
-
-class BusinessRequirementRenew(models.Model):
-    def copy(self):
-        pass
