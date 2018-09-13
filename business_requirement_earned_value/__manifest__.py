705965feb6f63e0debd44bf5a3d587158f485435
Hulk Liu - Elico Corp <hulk.liu@elico-corp.com>
2018-09-13 09:27:46 +0800

    [ADD]business_requirement_earned_value (#278)
    * business_requirement_earned_value
    
    * [IMP]make code more standard
    
    * [IMP]edit the license and add the badge
    
    * [IMP]fix the badge image and add the contributor
    
    * [FIX]Upgrade to a new version
    
    * [FIX]fix some travis error and copyright error
    
    * [FIX]fix test file
    
    * [FIX]add author elico
    
    * [FIX]travis error
    
    * [IMP]add screenshot in readme
    
    * [FIX]change the screenshot
    
    * [FIX]change the menu and del the wrong fiel
    
    * [FIX]add demo data
    
    * [FIX]change the image
    
    * [FIX]change the demo data and fix the sql
    
    * [FIX]Fix the conversion rate
    
    * [IMP]change the screenshot
    
    * [FIX]change the  conversion rate
    
    * [FIX]fix the travis error
    
    * [FIX]demo data error
    
    * [FIX]tavis error
    
    * [FIX]add README introduction
    
    * [FIX]change the screenshot
    
    * [FIX]readme
    
    * [FIX]demo data error
    
    * [FIX]demo data
    
    * [ADD]add demo data
    
    * [FIX]demo data
    
    * [FIX]change readme image

--- business_requirement_earned_value/__manifest__.py
+++ business_requirement_earned_value/__manifest__.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 # © 2017 Elico Corp (https://www.elico-corp.com).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Earned Value Management',
-    'category': 'Business Requirements Management',
-    'summary': 'Manage the Earned Value for your customers',
-    'version': '8.0.1.0.0',
-    'website': 'https://www.elico-corp.com/',
-    'author': 'Elico Corp, Odoo Community Association (OCA)',
-    'depends': [
-        'hr_timesheet',
-        'project_timesheet',
-        'business_requirement_deliverable_cost',
-        'business_requirement_deliverable_project',
+    "name": "Business Requirement Earned Value",
+    "summary": "Follow-up project completion (estimated / consumed)",
+    "version": "10.0.1.0.0",
+    "category": "Project",
+    'website': 'https://github.com/OCA/business-requirement/',
+    'support': 'support@elico-corp.com',
+    "author": "Elico Corp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "hr_timesheet",
+        "project_issue_sheet",
+        "project_task_category",
+        "business_requirement_deliverable_project",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'report/br_earned_value_report_view.xml',
+    "data": [
+        "report/project_completion_report_view.xml",
+        "security/ir.model.access.csv",
+        'views/br_and_project_inherit_view.xml',
     ],
-    'license': 'AGPL-3',
-    'installable': False,
+    'demo': [
+        'demo/project_completion_report_demo.xml'
+    ]
 }
---
+++ business_requirement_earned_value/tests/test_report_services.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2017 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo.tests import common
+
+
+class TestReportService(common.TransactionCase):
+    def setUp(self):
+        super(TestReportService, self).setUp()
+        self.br = self.env.ref('business_requirement.business_requirement_4')
+        self.project = self.env.ref('project.project_project_4')
+
+    def test_br_open_project_completion_report(self):
+        res = self.br.br_open_project_completion_report()
+        self.assertTrue(res['name'], 'BR Project completion report')
+
+    def test_project_open_pro_com_trp(self):
+        res = self.project.project_open_pro_com_trp()
+        self.assertTrue(res['name'], 'Project project completion report')
