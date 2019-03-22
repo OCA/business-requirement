905514e97d6a7dd96c3ba6e87cab175f1a300179
Ruter Lv <ruter.lv@elico-corp.com>
2019-03-22 11:07:37 +0800

    [MIG] migrate from 10.0 to 12.0

--- business_requirement_deliverable_resource_template/__manifest__.py
+++ business_requirement_deliverable_resource_template/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
-# © 2017 Elico Corp (https://www.elico-corp.com).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2016-2019 Elico Corp (https://www.elico-corp.com).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Business Requirement Deliverable Resource Template',
     'category': 'Business Requirements Management',
     'summary': """Manage default resource lines in your
                deliverable sales package from product template""",
-    'version': '10.0.1.0.0',
-    'website': 'https://www.elico-corp.com/',
+    'version': '12.0.1.0.0',
+    'website': 'https://github.com/OCA/business-requirement',
     "author": "Elico Corp, Odoo Community Association (OCA)",
     'depends': [
         'business_requirement_deliverable',
@@ -24,6 +23,6 @@
     'demo': [
         'demo/business_requirement_deliverable_resource_template_demo.xml'
     ],
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'installable': True,
 }
--- business_requirement_deliverable_resource_template/models/business_requirement_resource_template.py
+++ business_requirement_deliverable_resource_template/models/business_requirement_resource_template.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# © 2017 Elico Corp (https://www.elico-corp.com).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2016-2019 Elico Corp (https://www.elico-corp.com).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
@@ -42,8 +41,7 @@ class BusinessRequirementResourceTemplate(models.Model):
     name = fields.Char('Name', required=True)
     product_id = fields.Many2one(
         comodel_name='product.product',
-        string='Product',
-        required=False
+        string='Product'
     )
     uom_id = fields.Many2one(
         comodel_name='product.uom',
