851c1e286760449ed04c24bbe1caada22c975320
Ruter Lv <ruter.lv@elico-corp.com>
2019-03-22 11:24:15 +0800

    [MIG] migrate from 10.0 to 12.0

--- business_requirement_etherpad/__manifest__.py
+++ business_requirement_etherpad/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-# © 2017 Elico Corp (https://www.elico-corp.com).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2016-2019 Elico Corp (https://www.elico-corp.com).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Business Requirement Etherpad',
     'category': 'Business Requirements Management',
     'summary': 'Manage the Business Requirements Notes via Etherpad',
-    'version': '10.0.1.0.0',
-    'website': 'https://www.elico-corp.com/',
+    'version': '12.0.1.0.0',
+    'website': 'https://github.com/OCA/business-requirement',
     "author": "Elico Corp, Odoo Community Association (OCA)",
     'depends': [
         'pad',
@@ -18,6 +17,6 @@
     'demo': [
         'data/pad_demo.xml',
     ],
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'installable': True,
 }
