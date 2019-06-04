4e749b2039400cbc68601dfeab7835c4b29e40f9
Victor Martin <victor.martin@tecnativa.com>
2019-06-04 16:47:45 +0200

    [MIG] business_requirement: Migration to 11.0

--- business_requirement/tests/test_br.py
+++ business_requirement/tests/test_br.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2017 Elico Corp (https://www.elico-corp.com).
+# © 2017-2019 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo.tests import common
 from openerp.exceptions import ValidationError
