9db48b80ae71df8c7cb94ab27c1ccda0e390fda6
Victor Martin <victor.martin@tecnativa.com>
2019-06-06 15:48:38 +0200

    [MIG] business_requirement_deliverable: Migration to 11.0

--- business_requirement_deliverable/__manifest__.py
+++ business_requirement_deliverable/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-# © 2016-2017 Elico Corp (https://www.elico-corp.com).
+# © 2016-2019 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Business Requirement Deliverable',
     'category': 'Business Requirements Management',
-    'summary': 'Manage the Business Requirement Deliverables and \
-                Resources for your customers',
-    'version': '10.0.1.0.0',
+    'summary': 'Manage the Business Requirement Deliverables \
+                for your customers',
+    'version': '11.0.1.0.0',
     'website': 'https://www.elico-corp.com/',
     "author": "Elico Corp, Odoo Community Association (OCA)",
     'depends': [
@@ -15,22 +14,20 @@
     ],
     'data': [
         'data/business_data.xml',
-        'data/br_report_paperformat.xml',
+        'data/brd_report_paperformat.xml',
         'security/business_requirement_deliverable_security.xml',
         'security/ir.model.access.csv',
         'views/business_view.xml',
         'views/res_partner.xml',
         'views/report_business_requirement_deliverable.xml',
-        'views/report_business_requirement_deliverable_resource.xml',
-        'report/br_deliverable_report_view.xml',
-        'report/br_deliverable_sale_report_view.xml',
+        'views/brd_portal_templates.xml',
+        'views/assets.xml',
         'report/report.xml',
     ],
     'image': [
         'static/description/icon.png',
         'static/img/bus_req_deliverable.png',
         'static/img/bus_req_deliverable2.png',
-        'static/img/bus_req_resource.png',
         'static/img/bus_req_report1.png',
         'static/img/bus_req_report2.png',
         'static/img/bus_req_report3.png',
