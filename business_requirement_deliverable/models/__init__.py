c2c68f981e0de96e58d819e066fa7fa4fb59335b
Victor Martin <victor.martin@tecnativa.com>
2019-06-06 15:48:38 +0200

    [MIG] business_requirement_deliverable: Migration to 11.0

--- business_requirement_deliverable/models/__init__.py
+++ business_requirement_deliverable/models/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2016-2017 Elico Corp (https://www.elico-corp.com).
+# © 2016-2019 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import business
