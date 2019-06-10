498fb060196001d0614136dcae990b0e5368ed72
Victor Martin <victor.martin@tecnativa.com>
2019-06-10 12:10:06 +0200

    [REM] Master project and WBS

--- business_requirement/models/business.py
+++ business_requirement/models/business.py
@@ -7,10 +7,10 @@ from odoo.exceptions import ValidationError
 
 
 class BusinessRequirement(models.Model):
-    _inherit = ['mail.thread', 'portal.mixin']
+    _inherit = ["mail.thread", "mail.activity.mixin", "portal.mixin"]
     _name = "business.requirement"
     _description = "Business Requirement"
-    _order = 'name desc'
+    _order = "name desc"
 
     @api.model
     def _get_default_company(self):
@@ -27,7 +27,6 @@ class BusinessRequirement(models.Model):
     )
     name = fields.Char(
         'Name',
-        required=False,
         readonly=True,
         copy=False,
         states={'draft': [('readonly', False)]}
@@ -35,13 +34,6 @@ class BusinessRequirement(models.Model):
     description = fields.Char(
         'Description', required=True,
         readonly=True,
-        states={'draft': [('readonly', False)]}
-    )
-    ref = fields.Char(
-        'WBS',
-        required=False,
-        readonly=True,
-        copy=False,
         states={'draft': [('readonly', False)]}
     )
     business_requirement = fields.Html(
@@ -81,7 +73,7 @@ class BusinessRequirement(models.Model):
         states={'draft': [('readonly', False)]}
     )
     state = fields.Selection(
-        selection="_get_states",
+        selection=lambda self: self._selection_get_states(),
         string='State',
         default='draft',
         copy=False,
@@ -89,54 +81,17 @@ class BusinessRequirement(models.Model):
         states={'draft': [('readonly', False)]},
         track_visibility='onchange'
     )
-    business_requirement_ids = fields.One2many(
-        comodel_name='business.requirement',
-        inverse_name='parent_id',
-        string='Sub Business Requirement',
-        copy=False,
-        readonly=True,
-        states={'draft': [('readonly', False)]}
-    )
-    parent_id = fields.Many2one(
-        comodel_name='business.requirement',
-        string='Parent',
-        ondelete='set null',
-        domain="[('id', '!=', id)]",
-        readonly=True,
-        states={
-            'draft': [('readonly', False)],
-            'confirmed': [('readonly', False)]}
-    )
-    level = fields.Integer(
-        compute='_compute_get_level',
-        string='Level',
-        store=True
-    )
     change_request = fields.Boolean(
         string='Change Request?',
-        default=False,
-        readonly=True,
-        states={'draft': [('readonly', False)]}
-    )
-    project_id = fields.Many2one(
-        comodel_name='project.project',
-        string='Master Project',
-        ondelete='set null',
-        copy=False,
         readonly=True,
         states={'draft': [('readonly', False)]}
     )
     partner_id = fields.Many2one(
         comodel_name='res.partner',
         string='Stakeholder',
-        store=True,
         copy=False,
         readonly=True,
         states={'draft': [('readonly', False)]}
-    )
-    sub_br_count = fields.Integer(
-        string='Count',
-        compute='_compute_sub_br_count'
     )
     priority = fields.Selection(
         [('0', 'Low'), ('1', 'Normal'), ('2', 'High')],
@@ -144,7 +99,7 @@ class BusinessRequirement(models.Model):
         required=True,
         default='1'
     )
-    requested_id = fields.Many2one(
+    requested_user_id = fields.Many2one(
         'res.users',
         string='Requested by',
         required=True,
@@ -159,12 +114,12 @@ class BusinessRequirement(models.Model):
         copy=False,
         readonly=True
     )
-    confirmed_id = fields.Many2one(
+    confirmed_user_id = fields.Many2one(
         'res.users', string='Confirmed by',
         copy=False,
         readonly=True
     )
-    responsible_id = fields.Many2one(
+    responsible_user_id = fields.Many2one(
         'res.users', string='Responsible',
         copy=False,
         readonly=True,
@@ -210,7 +165,6 @@ class BusinessRequirement(models.Model):
         ],
         'Kanban State',
         track_visibility='onchange',
-        required=False,
         default='normal'
     )
     origin = fields.Char(
@@ -225,72 +179,33 @@ class BusinessRequirement(models.Model):
     user_id = fields.Many2one('res.users', string='Owner',
                               default=lambda self: self.env.user,
                               required=True)
-    date = fields.Date('Date', default=lambda self: self._context.get(
-                           'date', fields.Date.context_today(self)),
-                       required=True)
-
-    @api.multi
-    @api.onchange('project_id')
-    def project_id_change(self):
-        if self.project_id and self.project_id.partner_id:
-            self.partner_id = self.project_id.partner_id.id
+    date = fields.Date(
+        'Date',
+        default=lambda self: self._context.get(
+            'date', fields.Date.context_today(self)),
+        required=True
+    )
 
     @api.model
     def create(self, vals):
         if vals.get('name', '/') == '/':
             vals['name'] = self.env['ir.sequence'].next_by_code(
                 'business.requirement')
-        if vals.get('project_id'):
-            project_id = self.env['project.project']. \
-                browse(vals.get('project_id'))
-            if project_id and project_id.message_follower_ids:
-                msg_followers = []
-                for project in project_id.message_follower_ids:
-                    msg_vals = {
-                        'channel_id': project.channel_id.id,
-                        'display_name': project.display_name,
-                        'partner_id': project.partner_id.id,
-                        'res_model': self._name,
-                        'subtype_ids': project.subtype_ids.ids
-                    }
-                    msg_followers.append((0, 0, msg_vals))
-                if msg_followers:
-                    vals['message_follower_ids'] = msg_followers
         return super(BusinessRequirement, self).create(vals)
 
     @api.multi
     def write(self, vals):
-        if vals.get('project_id'):
-            project_id = self.env['project.project']. \
-                browse(vals.get('project_id'))
-            if project_id and project_id.message_follower_ids:
-                msg_followers = []
-                for followers in self.message_follower_ids:
-                    msg_followers.append((2, followers.id))
-                for project in project_id.message_follower_ids:
-                    msg_vals = {
-                        'channel_id': project.channel_id.id,
-                        'display_name': project.display_name,
-                        'partner_id': project.partner_id.id,
-                        'res_model': self._name,
-                        'subtype_ids': project.subtype_ids.ids
-                    }
-                    msg_followers.append((0, 0, msg_vals))
-                if msg_followers:
-                    vals['message_follower_ids'] = msg_followers
         if vals.get('state'):
-            ir_obj = self.env['ir.model.data']
-            br_xml_id = ir_obj.\
-                get_object('business_requirement',
-                           'group_business_requirement_manager')
+            br_xml_id = self.env.ref(
+                'business_requirement.group_business_requirement_manager')
             user = self.env['res.users']
-            grps = [grp.id for grp in user.browse(self._uid).groups_id]
+            grps = self.env.user.groups_id.ids
             date = fields.Datetime.now()
             if vals['state'] == 'confirmed':
-                vals.update({'confirmed_id': user,
+                vals.update({'confirmed_user_id': user,
                              'confirmation_date': date})
             if vals['state'] == 'draft':
-                vals.update({'confirmed_id': False,
+                vals.update({'confirmed_user_id': False,
                              'approved_id': False,
                              'confirmation_date': False,
                              'approval_date': False
@@ -303,18 +218,7 @@ class BusinessRequirement(models.Model):
                     raise ValidationError(_(
                         'You can only move to the following stage: '
                         'draft/confirmed /cancel/drop.'))
-            if vals['state'] == 'stakeholder_approval':
-                if br_xml_id.id in grps:
-                    vals.update({
-                        'approved_id': user,
-                        'approval_date': date
-                    })
-                else:
-                    raise ValidationError(_(
-                        'You can only move to the following stage: '
-                        'draft/confirmed /cancel/drop.'))
-
-            if vals['state'] in ('stakeholder_approval', 'in_progress',
+            if vals['state'] in ('approved', 'in_progress',
                                  'done'):
                 if br_xml_id.id not in grps:
                     raise ValidationError(_(
@@ -322,26 +226,12 @@ class BusinessRequirement(models.Model):
                         'draft/confirmed/cancel/drop.'))
         return super(BusinessRequirement, self).write(vals)
 
-    @api.multi
-    @api.depends('parent_id')
-    def _compute_get_level(self):
-        for br in self:
-            level = br.parent_id and br.parent_id.level + 1 or 1
-            br.level = level
-
-    @api.multi
-    @api.depends('business_requirement_ids')
-    def _compute_sub_br_count(self):
-        for br in self:
-            br.sub_br_count = len(br.business_requirement_ids)
-
     @api.model
-    def _get_states(self):
+    def _selection_get_states(self):
         states = [
             ('draft', 'Draft'),
             ('confirmed', 'Confirmed'),
             ('approved', 'Approved'),
-            ('stakeholder_approval', 'Stakeholder Approval'),
             ('in_progress', 'In progress'),
             ('done', 'Done'),
             ('cancel', 'Cancel'),
@@ -352,16 +242,11 @@ class BusinessRequirement(models.Model):
     @api.multi
     def name_get(self):
         """
-        Display [Reference] Description if reference is defined
-        otherwise display [Name] Description
+        Display display [Name] Description
         """
         result = []
         for br in self:
-            if br.ref:
-                formatted_name = u'[{}][{}] {}'.format(br.ref, br.name,
-                                                       br.description)
-            else:
-                formatted_name = u'[{}] {}'.format(br.name, br.description)
+            formatted_name = u'[{}] {}'.format(br.name, br.description)
             result.append((br.id, formatted_name))
         return result
 
