873f327922069d2f59e567ba1a2e7c4f57185d04
Victor Martin <victor.martin@tecnativa.com>
2019-06-10 12:10:06 +0200

    [REM] Master project and WBS

--- business_requirement/tests/test_br.py
+++ business_requirement/tests/test_br.py
@@ -10,9 +10,7 @@ from odoo import _
 class BusinessRequirementTestCase(common.TransactionCase):
     def setUp(self):
         super(BusinessRequirementTestCase, self).setUp()
-        self.ProjectObj = self.env['project.project']
 
-        self.AnalyticAccountObject = self.env['account.analytic.account']
         # Configure unit of measure.
         self.categ_wtime = self.ref('product.uom_categ_wtime')
         self.categ_kgm = self.ref('product.product_uom_categ_kgm')
@@ -35,19 +33,10 @@ class BusinessRequirementTestCase(common.TransactionCase):
             'uom_type': 'reference',
             'rounding': 0.000001})
 
-        self.AnalyticAccount = self.AnalyticAccountObject.create(
-            {'name': 'AnalyticAccount for Test'})
-
-        self.projectA = self.ProjectObj. \
-            create({'name': 'Test Project A', 'partner_id': 1,
-                    'analytic_account_id': self.AnalyticAccount.id})
         self.br = self.env['business.requirement']
-        self.pr_1 = self.env.ref('project.project_project_1')
-        self.pr_2 = self.env.ref('project.project_project_2')
 
         vals = {
             'description': 'test',
-            'project_id': self.projectA.id
         }
         # Product Created A, B, C, D
         self.ProductObj = self.env['product.product']
@@ -62,24 +51,9 @@ class BusinessRequirementTestCase(common.TransactionCase):
 
         vals1 = vals.copy()
         vals2 = vals.copy()
-        self.brA = self.env['business.requirement'].create(vals)
-        self.brB = self.env['business.requirement'].create(vals1)
-        self.brC = self.env['business.requirement'].create(vals2)
-
-    def test_project_id_change(self):
-        self.pr_1.write({'partner_id': self.partner1})
-        self.brA.project_id_change()
-        self.assertEqual(
-            self.brA.project_id.partner_id.id, self.brA.partner_id.id
-        )
-
-    def test_compute_sub_br_count(self):
-        self.brC.write({'parent_id': self.brA.id})
-        self.brB.write({'parent_id': self.brA.id})
-        self.brA._compute_sub_br_count()
-        self.assertEqual(
-            self.brA.sub_br_count, len(self.brA.business_requirement_ids)
-        )
+        self.brA = self.br.create(vals)
+        self.brB = self.br.create(vals1)
+        self.brC = self.br.create(vals2)
 
     def test_message_post(self):
         self.brA.with_context({
@@ -101,43 +75,10 @@ class BusinessRequirementTestCase(common.TransactionCase):
         with self.assertRaises(ValidationError):
             self.brA._get_default_company()
 
-    def test_get_level(self):
-        br_vals1 = {
-            'name': ' test',
-            'description': 'test',
-            'ref': '/',
-            'parent_id': False,
-        }
-        br1 = self.br.create(br_vals1)
-        br1._compute_get_level()
-        level1 = br1.level
-        self.assertEqual(level1, 1)
-
-        br_vals2 = {
-            'name': ' test',
-            'description': 'test',
-            'parent_id': br1.id,
-        }
-        br2 = self.br.create(br_vals2)
-        br2._compute_get_level()
-        level2 = br2.level
-        self.assertEqual(level2, 2)
-
-        br_vals3 = {
-            'name': ' test',
-            'description': 'test',
-            'parent_id': br2.id,
-        }
-        br3 = self.br.create(br_vals3)
-        br3._compute_get_level()
-        level3 = br3.level
-        self.assertEqual(level3, 3)
-
     def test_br_name_search(self):
         br_vals = {
             'name': ' test',
             'description': 'test',
-            'parent_id': False,
         }
         self.br.create(br_vals)
 
@@ -155,19 +96,6 @@ class BusinessRequirementTestCase(common.TransactionCase):
         res = self.br.create(br_vals)
         self.assertEqual(seq, res.name)
 
-    def test_create_write_project(self):
-        br_vals = {
-            'name': 'test',
-            'description': 'test',
-            'project_id': self.pr_1.id
-        }
-        res = self.br.create(br_vals)
-        br_vals1 = {
-            'project_id': self.pr_2.id
-        }
-        res.write(br_vals1)
-        self.assertEqual(res.project_id.id, self.pr_2.id)
-
     def test_br_read_group(self):
         self.env['business.requirement'].read_group(
             [],
@@ -175,31 +103,3 @@ class BusinessRequirementTestCase(common.TransactionCase):
         self.env['business.requirement'].read_group(
             [],
             [], [])[0]
-
-    def test_br_state_generate_project_wizard(self):
-        # test when state=draft
-        self.brA.state = 'draft'
-        self.brB.state = 'draft'
-        self.brC.state = 'draft'
-
-        # test when state=confirmed
-        self.brA.state = 'confirmed'
-        self.brB.state = 'confirmed'
-        self.brC.state = 'confirmed'
-
-        self.brB.state = 'confirmed'
-        self.brC.state = 'draft'
-
-        # test when state=stakeholder_approval
-        self.brA.state = 'stakeholder_approval'
-        self.brB.state = 'approved'
-        self.brC.state = 'approved'
-
-        # test when state=done
-        self.brA.state = 'done'
-        self.brB.state = 'approved'
-        self.brC.state = 'approved'
-
-        # test when state=cancel
-        self.brB.state = 'approved'
-        self.brC.state = 'approved'
