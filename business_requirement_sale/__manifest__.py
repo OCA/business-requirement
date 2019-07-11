4ef566d5c2e7430ecff82f46040d524e9dde3daa
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-07-12 01:37:45 +0200

    [IMP] business_requirement_sale: revamp
    * Refactorization
    * Method optimization
    * Tests
    * Translations

--- business_requirement_sale/__manifest__.py
+++ business_requirement_sale/__manifest__.py
@@ -3,23 +3,19 @@
 {
     "name": "Business Requirement Sale",
     'category': 'Business Requirements Management',
-    'summary': 'Convert Business Requirement into \
-                Sale Orders',
+    'summary': 'Convert Business Requirement into Sales Orders',
     "version": "11.0.1.0.0",
-    "development_status": "Alpha",
     "website": "https://github.com/OCA/business-requirement",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "application": False,
     "installable": True,
     "depends": [
         "business_requirement_deliverable",
         "sale_management",
     ],
     "data": [
-        'security/ir.model.access.csv',
-        'wizard/convert_requirement_sale.xml',
+        'wizard/business_requirement_create_sale_views.xml',
         'views/sale_order_views.xml',
         'views/business_requirement_views.xml',
     ],
