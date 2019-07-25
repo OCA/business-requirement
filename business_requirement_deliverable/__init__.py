603e450c18f925e2afbbdd0615c2997c6b799beb
Victor M.M. Torres <victor.martin@tecnativa.com>
2019-07-25 19:22:16 +0200

    [MIG] business_requirement_deliverable: Migration to v12

--- business_requirement_deliverable/__init__.py
+++ business_requirement_deliverable/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2016-2017 Elico Corp (https://www.elico-corp.com).
+# Copyright 2016-2017 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import models
 from . import tests
