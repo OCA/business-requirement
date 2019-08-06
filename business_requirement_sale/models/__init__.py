6e2caa32d2015d016653570424a47d3f0ce122ff
Victor M.M. Torres <victor.martin@tecnativa.com>
2019-08-06 17:25:05 +0200

    [MIG] business_requirement_sale: Migration to v12

--- business_requirement_sale/models/__init__.py
+++ business_requirement_sale/models/__init__.py
@@ -1,6 +1,5 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import business_requirement
-from . import business_requirement_deliverable
-from . import sale_layout_category
+from . import sale_order_line
 from . import sale_order
