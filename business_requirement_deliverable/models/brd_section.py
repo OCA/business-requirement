6eff9d3e2d325efb829d5710a3194db8dea56768
Victor M.M. Torres <victor.martin@tecnativa.com>
2019-08-06 17:25:05 +0200

    [MIG] business_requirement_sale: Migration to v12

--- business_requirement_deliverable/models/brd_section.py
+++ business_requirement_deliverable/models/brd_section.py
@@ -6,5 +6,7 @@ from odoo import fields, models
 class BusinessRequirementDeliverableSection(models.Model):
     _name = "business.requirement.deliverable.section"
     _description = "Deliverable Section"
+    _order = "sequence, id"
 
     name = fields.Char('Name', required=True)
+    sequence = fields.Integer('Sequence')
