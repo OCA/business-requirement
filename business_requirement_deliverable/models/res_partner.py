f6c1fab42e96cfba7d4942cf4dcf2c1d71b4245f
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-01-28 00:14:32 +0100

    [IMP] business_requirement_deliverable: black, isort, prettier

--- business_requirement_deliverable/models/res_partner.py
+++ business_requirement_deliverable/models/res_partner.py
@@ -4,17 +4,19 @@ from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     property_product_estimation_pricelist = fields.Many2one(
-        string='Estimation Pricelist',
-        comodel_name='product.pricelist',
+        string="Estimation Pricelist",
+        comodel_name="product.pricelist",
         company_dependent=True,
         help="""Pricelist used for the estimation of the Business Requirements
         Deliverables linked to this Customer.
-        Currency of the Deliverables will be the one from the pricelist.""")
+        Currency of the Deliverables will be the one from the pricelist.""",
+    )
 
     @api.model
     def _commercial_fields(self):
-        return super(ResPartner, self)._commercial_fields() + \
-            ['property_product_estimation_pricelist']
+        return super(ResPartner, self)._commercial_fields() + [
+            "property_product_estimation_pricelist"
+        ]
