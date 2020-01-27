4e09fd430df355c81e04d5778823619f63906d35
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-01-27 22:08:25 +0100

    [IMP] business_requirement_crm: black, isort

--- business_requirement_crm/wizard/crm_lead_convert_to_requirement.py
+++ business_requirement_crm/wizard/crm_lead_convert_to_requirement.py
@@ -2,7 +2,7 @@
 # Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 
 
 class CrmLeadCreateRequirement(models.TransientModel):
@@ -10,36 +10,34 @@ class CrmLeadCreateRequirement(models.TransientModel):
     _description = "Crm Lead Create Requirement"
 
     lead_id = fields.Many2one(
-        comodel_name='crm.lead',
-        string='Opportunity',
+        comodel_name="crm.lead",
+        string="Opportunity",
         readonly=True,
-        domain=[('type', '=', 'opportunity')])
-    description = fields.Char(
-        'Description',
-        required=True,
+        domain=[("type", "=", "opportunity")],
     )
-    customer_history = fields.Html('Customer history')
+    description = fields.Char("Description", required=True)
+    customer_history = fields.Html("Customer history")
 
     @api.model
     def default_get(self, fields):
         result = super(CrmLeadCreateRequirement, self).default_get(fields)
-        lead = self.env['crm.lead'].browse(
-            [self.env.context.get('active_id')])
+        lead = self.env["crm.lead"].browse([self.env.context.get("active_id")])
         if lead:
-            result.update({
-                'lead_id': lead.id,
-                'description': lead.name,
-                'customer_history': lead.description,
-            })
+            result.update(
+                {
+                    "lead_id": lead.id,
+                    "description": lead.name,
+                    "customer_history": lead.description,
+                }
+            )
         return result
 
     def _prepare_business_requirement_vals(self):
         return {
-            "business_requirement": (
-                self.customer_history or self.lead_id.description
-            ) or self.lead_id.name,
-            "description": self.description or (
-                self.lead_id.description or self.lead_id.name),
+            "business_requirement": (self.customer_history or self.lead_id.description)
+            or self.lead_id.name,
+            "description": self.description
+            or (self.lead_id.description or self.lead_id.name),
             "partner_id": self.lead_id.partner_id.id,
             "user_id": self.lead_id.user_id.id or self.lead_id.create_uid.id,
             "lead_id": self.lead_id.id,
@@ -52,13 +50,11 @@ class CrmLeadCreateRequirement(models.TransientModel):
         user, customer history, partner
         """
         self.ensure_one()
-        requirement = self.env['business.requirement'].create(
+        requirement = self.env["business.requirement"].create(
             self._prepare_business_requirement_vals()
         )
         # Chatter reflects new Requierement on both ways
-        msg_body = _(
-            "Requirement %s created"
-        ) % (
+        msg_body = _("Requirement %s created") % (
             "<a href=# data-oe-model=business.requirement data-oe-id=%d>%s</a>"
             % (requirement.id, requirement.name)
         )
@@ -66,10 +62,13 @@ class CrmLeadCreateRequirement(models.TransientModel):
         lead.message_post(body=msg_body)
         requirement_msg = _(
             "This business requirement has been created from:"
-        ) + " %s" % (
-            "<a href=# data-oe-model=crm.lead data-oe-id=%d>%s</a>"
-        ) % (lead.id, lead.name)
+        ) + " %s" % ("<a href=# data-oe-model=crm.lead data-oe-id=%d>%s</a>") % (
+            lead.id,
+            lead.name,
+        )
         requirement.message_post(body=requirement_msg)
-        return self.env[
-            'business.requirement'
-        ].browse(requirement.id).get_formview_action()
+        return (
+            self.env["business.requirement"]
+            .browse(requirement.id)
+            .get_formview_action()
+        )
