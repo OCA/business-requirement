9374d4d74791d3e6840af2141f7f11f3726fa987
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-01-27 21:38:16 +0100

    [IMP] business_requirement_sale_timesheet: black, isort

--- business_requirement_sale_timesheet/models/project_task.py
+++ business_requirement_sale_timesheet/models/project_task.py
@@ -5,24 +5,26 @@ from odoo import api, fields, models
 
 
 class ProjectTask(models.Model):
-    _inherit = 'project.task'
+    _inherit = "project.task"
 
     business_requirement_deliverable_id = fields.Many2one(
-        comodel_name='business.requirement.deliverable',
-        related='sale_line_id.business_requirement_deliverable_id',
+        comodel_name="business.requirement.deliverable",
+        related="sale_line_id.business_requirement_deliverable_id",
         readonly=True,
-        string='Deliverable',
+        string="Deliverable",
     )
 
     @api.multi
     def action_view_deliverable(self):
         action = self.env.ref(
-            'business_requirement_deliverable.action_deliverable_lines'
+            "business_requirement_deliverable.action_deliverable_lines"
         ).read()[0]
-        action.update({
-            'view_mode': 'form',
-            'views': [],
-            'view_id': False,
-            'res_id': self.business_requirement_deliverable_id.id,
-        })
+        action.update(
+            {
+                "view_mode": "form",
+                "views": [],
+                "view_id": False,
+                "res_id": self.business_requirement_deliverable_id.id,
+            }
+        )
         return action
