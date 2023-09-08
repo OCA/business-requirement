3782f64d86a1b1a527623ad09b39d099ae8fb839
Francisco V <fvasquez@fvtronics.com>
2023-09-08 10:26:48 -0400

    [ADD] module business_requirement_deliverable

---
+++ business_requirement_deliverable/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2016-2019 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import business
+from . import res_partner
+from . import brd_section
